005e8269bf3cccfc7af82585efb4f5dc5ec7e624
apadney9860 <apadney9860@gmail.com>
2020-11-27 17:15:58 +0545

    Form Created

---
+++ comp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap = new Bitmap(640, 480);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
