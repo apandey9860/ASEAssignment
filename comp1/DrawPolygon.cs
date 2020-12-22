1eb7724664263645b5e7c33d82d63efa12583880
apadney9860 <apadney9860@gmail.com>
2020-12-22 16:34:21 +0545

    Polygon can now be drawn All Component 2 requirements as well as additional features done Production Final Commit

---
+++ comp1/DrawPolygon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp1
+{
+    class DrawPolygon : Shape
+    {
+        PointF[] points;
+        /// <summary>
+        /// Set coordinates of polygon
+        /// </summary>
+        /// <param name="x">Coordinate of Polygon</param>
+        public DrawPolygon(int[] x) : base(x[0], x[1])
+        {
+            int i = 0;
+            PointF[] p = new PointF[x.Length];
+            while (x.Length > i)
+            {
+                if ((i + 1) == x.Length)
+                {
+                    p[i] = new PointF(x[i], x[0]);
+                }
+                else
+                {
+                    p[i] = new PointF(x[i], x[i + 1]);
+                }
+                i++;
+            }
+            points = p;
+        }
+        /// <summary>
+        /// Draw a polygon according to user defined coordinates
+        /// </summary>
+        /// <param name="canvass">Canvass in which the polygon is drawn</param>
+        public override void draw(Canvass canvass)
+        {
+            canvass.g.DrawPolygon(canvass.pen, points);//Draw a polygon
+            if (canvass.fill)
+            {
+                canvass.g.FillPolygon(canvass.brush, points);//Draws a filled polygon if fill is true
+            }
+        }
+    }
+}
